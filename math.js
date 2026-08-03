3d10744969baeb49bed668b61847bfb6cb24ead4
Your Name <you@example.com>
2026-08-03 23:05:47 +0600

    Learned basic math in js

---
+++ math.js
@@ -0,0 +1,31 @@
+//Add
+
+var  x = 33;
+var y = 43;
+var z = x+y;
+console.log(z);
+
+
+//Subtraction
+
+var  a = 300;
+var b = 97;
+var c = a-b;
+console.log(c);
+
+
+//shorthand
+var price = 35;
+price += 10;
+console.log(price)
+
+
+//Concatanation
+var first = "Mubarak";
+var second = "Tobarakj";
+var fullName = first+second;
+console.log(fullName)
+
+//Only for Addition
+
+console.log("40"+2);
