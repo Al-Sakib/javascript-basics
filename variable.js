5fc45e60d1927198e2b5ab2d07a84173893c983d
Your Name <you@example.com>
2026-08-03 22:49:11 +0600

    Learned JS Variable

---
+++ variable.js
@@ -0,0 +1,22 @@
+//Number Variable
+
+var weight = 38;
+var price = 400;
+console.log(weight);
+console.log(price);
+
+
+//String Variable
+
+var name = "Sakib";
+console.log(name);
+
+
+//Boolean type variable
+
+var isRich = true;
+var isPassed = false;
+console.log(isRich);
+console.log(isPassed);
+
+
